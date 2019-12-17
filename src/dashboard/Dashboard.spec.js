8c8f082fdb931ef4884b91670f3a0df7e2f7e2d1
William Sulinski <w@williamsulinski.com>
2019-12-16 16:30:35 -0800

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from "./Dashboard";
+
+test('Dashboard renders and shows controls and display', () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/unlocked/i);
+    getByText(/open/i);
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+})
+
